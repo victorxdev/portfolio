b21b47b757fa470d9d7d594800dff8fa84b5b44d
victorxdev <33047917+victorxdev@users.noreply.github.com>
2019-05-12 13:59:49 +0800

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -71,15 +71,15 @@ Ideation-Strategic-Restorative-Communication-Command (StrengthsFinder2.0)<br></b
           <form name="contact" method="POST" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name
-              <input type="text" name="name" /></label>
+              <input type="text" name="name" id="name"/></label>
             </div>
             <div className="field half">
               <label htmlFor="email">Email
-              <input type="email" name="email" /></label>
+              <input type="email" name="email" id="email" /></label>
             </div>
             <div className="field">
               <label htmlFor="message">Message
-              <textarea name="message" rows="4"></textarea></label>
+              <textarea name="message" rows="4" id="message"></textarea></label>
             </div>
             <ul className="actions">
               <li><input type="submit" value="Send"/></li>
